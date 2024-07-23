359236c01390650130739f5ea49d09917e1bc557
laurenlee156 <lauren16lee@gmail.com>
2024-07-22 21:45:02 -0700

    16th commit

--- part2.py
+++ part2.py
@@ -16,8 +16,19 @@ def question3(n1: dict, n2: dict) -> dict:
     return {key: n1.get(key, 0) + n2.get(key, 0) if key in n1 and key in n2 else n1.get(key, 0) or n2.get(key, 0)
                 for key in sorted(n1.keys()) + sorted(n2.keys())}
 def question4(n: list) -> list:
-    return [element for lst in range(0, len(n) - 1) for element in n[lst] if element in n[lst] and element in n[lst + 1]]
+
+    #return [element for lst in range(0, len(n) - 1) for element in lst if element in n[lst]]
+    #return [element for lst in range(0, len(n) - 1) for element in n[lst]
+            #if element in n[lst] and element in n[lst + 1]]
+    return [element for i in range(0, len(n) - 1) for j in range(i + 1, len(n))
+            for element in n[i] if element in n[j]]
+
+
+
 
 # a = question2({'a': 1, 'b': 0, 'c': 3, 'd': 4, 'e': 4})
-# b = question4([[1, 2], [3, 2], [1, 5, 3], [6, 5]])
+b = question4([[1, 2], [3, 2], [1, 5, 3], [6, 5]])
 # c = question3({'a': 1, 'b': 2}, {'a': 3, 'c': 2})
+print(b)
+
+
