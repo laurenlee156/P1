ea30f1a96bd82e6f3e2bc978ac31ca171f8c4788
laurenlee156 <lauren16lee@gmail.com>
2024-07-22 20:13:16 -0700

    13th commit

--- part6.py
+++ part6.py
@@ -15,16 +15,20 @@ class PeekableIterator:
     def peek(self):
         # get the next element in the iterator
         next_elem = next(self.iterable)
-        #self.iterable = iter(self.original)
+        self.iterable = iter(self.original)
         return next_elem
 
     def has_next(self):
-        if len(list(self.iterable)) > 0:
+        # if len(list(self.iterable)) > 0:
+        #     return True
+        # else:
+        #     return False
+        try:
+            next_elem = next(self.iterable)
+            self.iterable = iter(self.original)
             return True
-        else:
+        except StopIteration:
             return False
-
-
 
 # a = PeekableIterator([1, 2])
 # # # a.peek()
