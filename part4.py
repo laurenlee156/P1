826e295f37766675ad43e01849e54679708ecd92
laurenlee156 <lauren16lee@gmail.com>
2024-07-24 00:40:48 -0700

    31st commit

--- part4.py
+++ part4.py
@@ -3,24 +3,25 @@ def stable_stock_matching(buyers_preferences, stocks_preferences):
     first_key = list(buyers_preferences.keys())[0]
     # check what the first element of the buyer's preferences is and add to matched_dict
     matched_dict = {first_key: buyers_preferences[first_key][0]}
-
     buyers_preferences[first_key].remove(buyers_preferences[first_key][0])
+
 
     # remove the first key first value in every other value list
     for buyer in buyers_preferences:
         if buyer != first_key:
             for element in buyers_preferences[buyer]:
-                if element in matched_dict.values():
-                    buyers_preferences[buyer].remove(element)
-                else:
+                if element not in matched_dict.values():
                     matched_dict[buyer] = element
+                    break
+                else:
+                    buyers_preferences[buyer].remove(element)
 
-    return {value: key for key, value in matched_dict.items()}
+    return matched_dict
 
 
-# c = stable_stock_matching({"Buyer1" : ["StockA", "StockB", "StockC"],
-#                            "Buyer2" : ["StockB", "StockA", "StockC"],
-#                            "Buyer3" : ["StockA", "StockB", "StockC"]},
+# c = stable_stock_matching({"A" : [1, 2, 3],
+#                            "B" : [2, 1, 3],
+#                            "C" : [1, 2, 3]},
 #
 #                           {"StockA": ["Buyer1", "Buyer2", "Buyer3"],
 #                            "StockB": ["Buyer2", "Buyer1", "Buyer3"],
