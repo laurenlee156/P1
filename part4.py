a924a5179d05d3fa6b4b3f28659e7fd06a778a4c
laurenlee156 <lauren16lee@gmail.com>
2024-07-23 23:46:43 -0700

    30th commit

--- part4.py
+++ part4.py
@@ -18,13 +18,13 @@ def stable_stock_matching(buyers_preferences, stocks_preferences):
     return {value: key for key, value in matched_dict.items()}
 
 
-c = stable_stock_matching({"Buyer1" : ["StockA", "StockB", "StockC"],
-                           "Buyer2" : ["StockB", "StockA", "StockC"],
-                           "Buyer3" : ["StockA", "StockB", "StockC"]},
-
-                          {"StockA": ["Buyer1", "Buyer2", "Buyer3"],
-                           "StockB": ["Buyer2", "Buyer1", "Buyer3"],
-                           "StockC": ["Buyer1", "Buyer2", "Buyer3"]
-                        })
-
-print(c)
+# c = stable_stock_matching({"Buyer1" : ["StockA", "StockB", "StockC"],
+#                            "Buyer2" : ["StockB", "StockA", "StockC"],
+#                            "Buyer3" : ["StockA", "StockB", "StockC"]},
+#
+#                           {"StockA": ["Buyer1", "Buyer2", "Buyer3"],
+#                            "StockB": ["Buyer2", "Buyer1", "Buyer3"],
+#                            "StockC": ["Buyer1", "Buyer2", "Buyer3"]
+#                         })
+#
+# print(c)
