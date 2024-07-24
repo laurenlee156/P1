310f58b7445757217fd0783a16bd6671386ad09a
laurenlee156 <lauren16lee@gmail.com>
2024-07-24 16:22:41 -0700

    37th commit

--- part6.py
+++ part6.py
@@ -2,36 +2,38 @@ class PeekableIterator:
 
     def __init__(self, iterable):
         # initializes the iterator with the iterable
-        self.iterable = iter(iterable)
+        # self.iterable = iter(iterable)
         # assigns self.original as the parameter iterable
-        self.original = iterable
+        self.array = iterable
 
     def __iter__(self): # returns the iterator object
         return self
 
     def __next__(self):
-        return next(self.iterable)
+        if not self.array:
+            raise StopIteration
+        return self.array.pop(0)
+        # return next(self.iterable)
 
     def peek(self):
-        # get the next element in the iterator
-        next_elem = next(self.iterable)
-        # reverts the change
-        self.iterable = iter(self.original)
-        return next_elem
+        if self.array:
+            return self.array[0]
+        else:
+            raise StopIteration
 
     def has_next(self):
-        try:
-            if self.peek():
-                return True
-        except StopIteration:
-            #self.iterable = iter(self.original)
-            return False
+        return len(self.array) != 0
 
 
-# a = PeekableIterator([1])
-# print(a.peek())
-# print(a.has_next())
-# print(a.has_next())
+# a = PeekableIterator([1, 2])
+# print("has next: ", a.has_next())
+# print("peek: ",a.peek())
+# print("next: ",next(a))
+# print("has next: ", a.has_next())
+# print("peek: ",a.peek())
+# print("next: ",next(a))
+# print("has next: ", a.has_next())
+# print("peek: ",a.peek())
 
 
 
